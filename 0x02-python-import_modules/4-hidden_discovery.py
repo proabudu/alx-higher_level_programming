5babaaf19c009cd53c85554585537bbdf4961abc
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2023-12-04 05:27:47 +0000

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
+    '''Print all names defined by hidden_4 modules'''
     import hidden_4
 
     names = dir(hidden_4)
