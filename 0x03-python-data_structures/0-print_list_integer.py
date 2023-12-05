363364357e02b440048dae1c8d534e864af277d0
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2023-12-05 06:20:46 +0000

    changes to program

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
-for i in my_list:
-print('{:d}'.format(i))
+def print_list_integer(my_list=None):
+    if my_list is None:
+        my_list = []
 
+    for i in my_list:
+        print('{:d}'.format(i))
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
