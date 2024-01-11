ac1e5feedd20f2489423da5f6ffd93c5f89c3638
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-01-11 06:32:53 +0000

    try

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-    "" This module defines a text file-reading function ""
-
 def read_file(filename=""):
-    "" Prints the contents of a UTF8 text file. ""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """Reads a text file (UTF8) and prints its contents to stdout.
+
+    Args:
+        filename (str, optional): The name of the file to read. Defaults to "".
+    """
+
+    with open(filename, "r", encoding="utf-8") as file:
+        contents = file.read()
+        print(contents)
+
