7d883d30fd77100a8b1e0d524af5eb24ef730800
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-01-11 06:50:21 +0000

    3-to_json_string.py and del file 0

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as file: "" Open the file in read mode with UTF-8 encoding""
-contents = file.read()
-print(contents)
