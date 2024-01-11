127446f507a43a4e91afc31be16b23ae1bc144b3
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-01-11 07:05:52 +0000

    7 corr

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""This module adds all arguments to a Python list and save them to a file."""
-
-
 import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-"""This module adds all arguments to a Python list and save them to a file."""
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:]) """This module adds all arguments to a Python list and save them to a file."""
-    save_to_json_file(items, "add_item.json")
+# Create a list of arguments
+args = sys.argv[1:]
+
+# Load the existing list from the file, or create a new one if the file does not exist
+try:
+    my_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    my_list = []
+
+# Add the arguments to the list
+my_list.extend(args)
+
+# Save the list to the file
+save_to_json_file(my_list, "add_item.json")
