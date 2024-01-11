efe17a3b4d7dbbd93cb224c1f67837fa50bd0d7d
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-01-11 06:41:21 +0000

    again

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints its contents to stdout.
-
-    Args:
-        filename (str, optional): The name of the file to read. Defaults to "".
-    """
-
-    with open(filename, "r", encoding="utf-8") as file:
-        contents = file.read()
-        print(contents)
-
+    with open(filename, "r", encoding="utf-8") as file: "" Open the file in read mode with UTF-8 encoding""
+contents = file.read()
+print(contents)
