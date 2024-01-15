e1f229f1bb2dd0c39655eaf707df001182380e83
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-01-15 15:12:24 +0000

    aa

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""""
+Imports the 0-rectangle module and its Rectangle class for use.
+Creates a new instance of the Rectangle class named my_rectangle.
+"""
+
 Rectangle = __import__('0-rectangle').Rectangle
 
 my_rectangle = Rectangle()
