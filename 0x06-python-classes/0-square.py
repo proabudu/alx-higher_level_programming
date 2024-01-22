7492b807cde41a451b6a3bdf32c7da2e5d29c406
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-01-22 20:43:40 +0000

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Prosper Progress
+
+"""Defines a square """
+
+
+class Square:
+    """Does nothing"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+GitHub repository: alx-higher_level_programming
+Directory: 0x06-python-classes
+File: 0-square.py
