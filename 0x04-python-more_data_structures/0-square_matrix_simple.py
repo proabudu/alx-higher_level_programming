de7a2ee8ad3b5bd58e9e0d2b3f3fd9319582176a
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-01-22 16:52:42 +0000

    quick

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,24 +1,4 @@
 #!/usr/bin/python3
 
-"""
-    Create a new empty matrix of the same size
-"""
-
 def square_matrix_simple(matrix=[]):
-
-  # Check if matrix is a valid 2D list of integers
-  if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-    raise ValueError("Matrix must be a 2D list.")
-  for row in matrix:
-    if not all(isinstance(x, int) for x in row):
-      raise ValueError("Matrix must only contain integers.")
-
-
-  new_matrix = [[0 for _ in range(len(row))] for row in matrix]
-
-  # Loop through each element and replace it with its square in the new matrix
-  for i, row in enumerate(matrix):
-    for j, element in enumerate(row):
-      new_matrix[i][j] = element * element
-
-  return new_matrix
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
