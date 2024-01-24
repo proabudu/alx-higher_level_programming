f78a753217744157de784a2493689e704403b5e1
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-01-24 07:40:46 +0000

    task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-""""
-Imports the 0-rectangle module and its Rectangle class for use.
-Creates a new instance of the Rectangle class named my_rectangle.
-"""
+"""A class that defines a rectangle"""
 
-Rectangle = __import__('0-rectangle').Rectangle # # Import the Rectangle class from the '0-rectangle' module for rectangle creation and manipulation
 
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
+class Rectangle:
+    """An empty class that represents a rectangle"""
+    pass
