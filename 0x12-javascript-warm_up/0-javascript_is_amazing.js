96eb00a898b4620d6fae875bea3368b82b68b399
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-12 05:25:30 +0000

    dd

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 // Create a constant variable called myVar with the value "JavaScript is amazing"
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
 // Use console.log(...) to print all output
