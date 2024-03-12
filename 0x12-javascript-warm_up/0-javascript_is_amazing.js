2ee7bb0fc4b6b474308f43a8ba0ef6ec02b09967
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-12 05:32:50 +0000

    removing comments from my code and changing double quotes to single code

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 
-// Create a constant variable called myVar with the value "JavaScript is amazing"
 const myVar = 'JavaScript is amazing';
-console.log(myVar); // Use console.log(...) to print all output
+console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
-// Print the first line
-const line1 = "C is fun";
+const line1 = 'C is fun';
 console.log(line1);
 
-// Print the second line
-const line2 = "Python is cool";
+const line2 = 'Python is cool';
 console.log(line2);
 
-// Print the third line
-const line3 = "JavaScript is amazing";
+const line3 = 'JavaScript is amazing';
 console.log(line3);
