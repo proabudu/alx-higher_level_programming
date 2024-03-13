c7dd2927f532264bb2c33579dfdb46308020f90d
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-13 05:57:51 +0000

    d

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
+<<<<<<< HEAD
+function factorial (n) {
+=======
 
 function factorial(n) {
+>>>>>>> 27dc5cacce0c9d67dcdadd432adde77e50d0332f
   if (n === 0 || isNaN(n)) {
     return 1;
   }
@@ -12,6 +16,5 @@ function factorial(n) {
   }
   return result;
 }
-
 
 console.log(factorial(Number(process.argv[2])));
