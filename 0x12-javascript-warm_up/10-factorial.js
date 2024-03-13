4d1e2ea904606fc27f81268cdec85f3b2c53a7a1
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-03-13 06:02:46 +0000

    k

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
 // Function to calculate factorial of a number
-function factorial(n) {
-      // If n is 0 or NaN, return 1 (base case for factorial)
-      return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
-    } // End of factorial function
+function factorial(n)
+{
+  // If n is 0 or NaN, return 1 (base case for factorial)
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+} // End of factorial function
   
   // Get the input number from command line arguments and calculate its factorial
   console.log(factorial(Number(process.argv[2])));
