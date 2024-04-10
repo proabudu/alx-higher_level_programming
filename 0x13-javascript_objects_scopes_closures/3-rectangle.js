ea5bf13fe7ba90695634ab3de0fb4d66038c0861
proabudu <abuduprosperyao@gmail.com>
2024-04-10 09:36:59 +0000

    indentation fixes

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,24 +3,24 @@
  * Check the parameters provided
  */
 class Rectangle {
-    constructor (w, h) {
-      if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        let myVar = '';
-        let y = 0;
-        while (y < this.width) {
-          myVar += 'X';
-          y++;
-        }
-  
-        console.log(myVar);
-      }
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  module.exports = Rectangle;
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += 'X';
+        y++;
+      }
+
+      console.log(myVar);
+    }
+  }
+}
+module.exports = Rectangle;
