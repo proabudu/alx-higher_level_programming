3cce5f4e7dbd33fbabce858ea0f301e560130bab
PROSPER ABUDU <128247250+proabudu@users.noreply.github.com>
2024-04-12 12:50:24 +0000

    s

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -2,12 +2,12 @@
 """
 Lists all City objects from the database hbtn_0e_101_usa
 """
+
 import sys
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
